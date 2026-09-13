ffe39618a4d15056432b851948bed9303eeeae4e
VaterAbraham78 <dietschi.markus@deep.ch>
2026-09-13 15:43:58 +0200

    ChickenGateSytem_RevE_CorrV22
    Codeversion: CorrV22
    Nextionversion: V2

--- ChickenGateSytem_RevE.cpp
+++ ChickenGateSytem_RevE.cpp
@@ -1,12 +1,13 @@
 /**************************************************/
 /***	ChickenGateSystem Rev.E					***/
-/***	Korrekturversion: V21					***/
+/***	Korrekturversion: V22					***/
 /***											***/
 /***	Interrupt vorbereitet (OHNE Sleepmode)	***/
 /***	EEPROM-Magic-Byte fuer Plausibilitaet	***/
 /***	Sensorspeisung KEINE bistabilen Relais	***/
 /***	Reset-Taster: KEINE Multifunktion mehr	***/
 /***	Parameter via Touch-HMI-Eingabefeld		***/
+/***	Tastereingabe via Touch-HMI-Button		***/
 /***	HW-PWM "Innenbeleuchtung Licht-Stall"	***/
 /***	Korrektur Pinbelegung fuer Outputs		***/
 /***	UART-Kommunikation Nextion-Touchpanel	***/
@@ -107,7 +108,7 @@ int gwLightTime = DEF_LIGHTTIME;			// maximale Einschaltzeit "Licht Stall"				[i
 const int averageCnt = 10;					// Anzahl Zyklen fuer Mittelwertbildung
 int motfuseVolt = 0;						// aktuelle Spannung "RM Motorsicherung"
 int gwMotfuseVolt = 546;					// Grenzwert "RM Motorsicherung"					[546=8.00V=Sicherung ausgeloest]
-int batterieVolt = 0;						// aktuelle Batteriespannung
+int batterieVolt = 0;						// aktuelle Batteriewert
 int batterieProzent = 0;					// aktuelle Batterieladung in Prozent				[SOC, aus "batterieVolt" abgeleitet]
 int gwBatterieVolt = 810;					// Grenzwert "Batteriespannung tief"				[810=11.85V=30%]
 
@@ -137,34 +138,42 @@ const byte EEPROM_MAGIC_VALUE = 0xA5;		// Erkennungswert der Speicherstruktur
 /*******************************************************************************************************/
 /***	NEXTION-TOUCHDISPLAY	***/
 
-		// component-ID (byte, bei Touch-Events zurueckgemeldet) und component-Name (String, fuer "get"/"set"-Textbefehle) sind hier PLATZHALTER. 
-		// Sie muessen mit dem tatsaechlichen Nextion-Projekt (im Nextion Editor: Attribut "id" bzw. Objektname jeder Komponente) uebereinstimmen
-		// Jede Komponente muss im Editor "Sends Component ID" bei Touch Release aktiviert haben, damit ein 0x65-Ereignis eintrifft.
+		// component-ID (byte, bei Touch-Events zurueckgemeldet) und component-Name (String, fuer "get"/"set"-Textbefehle)... 
+		// ...muessen mit dem tatsaechlichen Nextion-Projekt (im Nextion Editor: Attribut "id" bzw. Objektname jeder Komponente) uebereinstimmen
+		// WICHTIG: Component-IDs werden vom Nextion-Editor pro Seite ab 0 automatisch vergeben und koennen seitenuebergreifend identisch sein...
+		// ...die Auswertung in nexFrameAuswerten() prueft deshalb IMMER Seite UND ID gemeinsam!
+		
+const byte NEX_PAGE_MAIN = 0;				// Seite "pageMain"
+const byte NEX_PAGE_DAYLIGHT = 1;			// Seite "pageDaylight"
+const byte NEX_PAGE_BOXLIGHT = 2;			// Seite "pageBoxlight"
+const byte NEX_PAGE_SWITCHES = 3;			// Seite "pageSwitches"
 
-const byte NEX_CID_GWTAG = 1;				// Component-ID Eingabefeld "Grenzwert Tag"
-const byte NEX_CID_GWNACHT = 2;				// Component-ID Eingabefeld "Grenzwert Nacht"
-const byte NEX_CID_MAXLIGHTTIME = 3;		// Component-ID Eingabefeld "max.Einschaltdauer Licht Stall"
-const byte NEX_CID_DIMM = 4;				// Component-ID Eingabefeld "Dimmstufe Licht Stall"
+const byte NEX_CID_GWTAG = 4;				// effektive Nextion-ID Eingabefeld "Grenzwert Tag"
+const byte NEX_CID_GWNACHT = 6;				// effektive Nextion-ID Eingabefeld "Grenzwert Nacht"
+const byte NEX_CID_MAXLIGHTTIME = 6;		// effektive Nextion-ID Eingabefeld "max.Einschaltdauer Licht Stall"
+const byte NEX_CID_DIMM = 4;				// effektive Nextion-ID Eingabefeld "Dimmstufe Licht Stall"
+const byte NEX_CID_LICHTTOGGLE = 7;			// effektive Nextion-ID Eingabefeld "Button Licht Stall"
 
-const char NEX_NAME_GWTAG[] = "n0";			// Objektname im Nextion-Projekt - Platzhalter
-const char NEX_NAME_GWNACHT[] = "n1";		// Objektname im Nextion-Projekt - Platzhalter
-const char NEX_NAME_MAXLIGHTTIME[] = "n2";	// Objektname im Nextion-Projekt - Platzhalter
-const char NEX_NAME_DIMM[] = "n3";			// Objektname im Nextion-Projekt - Platzhalter
+const char NEX_NAME_GWTAG[] = "pageDaylight.p1_nb04";			// effektiver Objektname Eingabefeld "Grenzwert Tag"
+const char NEX_NAME_GWNACHT[] = "pageDaylight.p1_nb05";			// effektiver Objektname Eingabefeld "Grenzwert Nacht"
+const char NEX_NAME_MAXLIGHTTIME[] = "pageBoxlight.p2_nb06";	// effektiver Objektname Eingabefeld "max.Einschaltdauer Licht Stall"
+const char NEX_NAME_DIMM[] = "pageBoxlight.p2_nb04";			// effektiver Objektname Eingabefeld "Dimmstufe Licht Stall"
 
-const char NEX_NAME_ACTDAYLIGHT[] = "n4";	// Objektname im Nextion-Projekt - Platzhalter (nur Anzeige)
-const char NEX_NAME_ACTSTATETAG[] = "n5";	// Objektname im Nextion-Projekt - Platzhalter (nur Anzeige)
-const char NEX_NAME_ACTMOTFUSE[] = "n6";	// Objektname im Nextion-Projekt - Platzhalter (nur Anzeige)
-const char NEX_NAME_ACTBATTVOLT[] = "n7";	// Objektname im Nextion-Projekt - Platzhalter (nur Anzeige)
-const char NEX_NAME_ACTBATTLEVEL[] = "n8";	// Objektname im Nextion-Projekt - Platzhalter (nur Anzeige)
-const char NEX_NAME_ACTSTATESWITCH[] = "n9";// Objektname im Nextion-Projekt - Platzhalter (nur Anzeige)
-const char NEX_NAME_ACTCYCLETIME[] = "n10";	// Objektname im Nextion-Projekt - Platzhalter (nur Anzeige)
+const char NEX_NAME_ACTDAYLIGHT[] = "pageDaylight.p1_nb02";		// effektiver Objektname Anzeigefeld "Rohwert Tageslicht"
+const char NEX_NAME_ACTSTATETAG[] = "pageDaylight.p1_nb03";		// effektiver Objektname Anzeigefeld "Tag/Nacht-Status"
+const char NEX_NAME_ACTMOTFUSE[] = "pageMain.p0_nb06";			// effektiver Objektname Anzeigefeld "Rohwert RM Motorsicherung"
+const char NEX_NAME_ACTBATTVOLT[] = "pageMain.p0_nb03";			// effektiver Objektname Anzeigefeld "Rohwert Batteriespannung"
+const char NEX_NAME_ACTBATTLEVEL[] = "pageMain.p0_nb04";		// effektiver Objektname Anzeigefeld "Prozentwert der Batterieladung"
+const char NEX_NAME_ACTSTATESWITCH[] = "pageSwitches.vaSwitch";	// effektiver Objektname Variable 	 "Schalterzustand der Inputs"
+const char NEX_NAME_ACTCYCLETIME[] = "pageDaylight.p1_nb07";	// effektiver Objektname Anzeigefeld "Zykluszeit der CPU"
 
-enum HMI_REQUEST {HMI_NONE, HMI_REQ_GWTAG, HMI_REQ_GWNACHT, HMI_REQ_DIMM, HMI_REQ_MAXLIGHTTIME};
-HMI_REQUEST hmiPendingRequest = HMI_NONE;	// aktuell offene "get"-Anfrage ans HMI
+enum HMI_REQUEST {HMI_NONE, HMI_REQ_GWTAG, HMI_REQ_GWNACHT, HMI_REQ_MAXLIGHTTIME, HMI_REQ_DIMM};
+HMI_REQUEST hmiPendingRequest = HMI_NONE;						// aktuell offene "get"-Anfrage ans HMI
 enum NEX_PARSE_STATE {NEX_WAIT_CMD, NEX_COLLECT_PAYLOAD, NEX_WAIT_TERM, NEX_SKIP_UNKNOWN};	// Zustaende des laengenbasierten Nextion-Parsers
-unsigned long hmiRequestTime = 0;			// Zeitpunkt der letzten "get"-Anfrage (fuer Timeout)
-unsigned long hmiRequestTimeout = 1000;		// Timeout in ms, falls HMI nicht antwortet
-unsigned long hmiSendTime = 2000;			// Sendefrequenz "hmiSend()" in Millisekunden
+bool hmiLichtToggleRequest = false;								// HMI-Taster "Licht Stall" (wirkt wie physischer Taster)
+unsigned long hmiRequestTime = 0;								// Zeitpunkt der letzten "get"-Anfrage (fuer Timeout)
+unsigned long hmiRequestTimeout = 1000;							// Timeout in ms, falls HMI nicht antwortet
+unsigned long hmiSendTime = 2000;								// Sendefrequenz "hmiSend()" in Millisekunden
 
 /*******************************************************************************************************/
 /*******************************************************************************************************/
@@ -690,17 +699,22 @@ void nexFrameAuswerten(byte* frame, byte len)	{
 	}
 
 	if ((frame[0] == 0x65) && (len >= 4))	{								// Touch-Ereignis: 0x65, page, component, event
-		byte compId = frame[2];
-		byte eventType = frame[3];
+		byte pageId = frame[1];												// Seite, auf der das Ereignis stattfand						/***CHANGE - wird jetzt ausgewertet
+		byte compId = frame[2];												// Objekt-ID (wird von Nextion fix vergeben)
+		byte eventType = frame[3];											// Event-Typ
 
 		if (eventType == 0x00)	{											// Nur bei "Loslassen" reagieren (Wert steht dann fest)
 			HMI_REQUEST req = HMI_NONE;
 			const char* name = nullptr;
 
-			if (compId == NEX_CID_GWTAG)			{ req = HMI_REQ_GWTAG;			name = NEX_NAME_GWTAG;	}
-			else if (compId == NEX_CID_GWNACHT)		{ req = HMI_REQ_GWNACHT;		name = NEX_NAME_GWNACHT;	}
-			else if (compId == NEX_CID_MAXLIGHTTIME){ req = HMI_REQ_MAXLIGHTTIME;	name = NEX_NAME_MAXLIGHTTIME;	}
-			else if (compId == NEX_CID_DIMM)		{ req = HMI_REQ_DIMM;			name = NEX_NAME_DIMM;	}
+			if ((pageId == NEX_PAGE_DAYLIGHT) && (compId == NEX_CID_GWTAG))				{ req = HMI_REQ_GWTAG;			name = NEX_NAME_GWTAG;	}
+			else if ((pageId == NEX_PAGE_DAYLIGHT) && (compId == NEX_CID_GWNACHT))		{ req = HMI_REQ_GWNACHT;		name = NEX_NAME_GWNACHT;	}
+			else if ((pageId == NEX_PAGE_BOXLIGHT) && (compId == NEX_CID_MAXLIGHTTIME))	{ req = HMI_REQ_MAXLIGHTTIME;	name = NEX_NAME_MAXLIGHTTIME;	}
+			else if ((pageId == NEX_PAGE_BOXLIGHT) && (compId == NEX_CID_DIMM))			{ req = HMI_REQ_DIMM;			name = NEX_NAME_DIMM;	}
+			else if ((pageId == NEX_PAGE_BOXLIGHT) && (compId == NEX_CID_LICHTTOGGLE))	{
+				hmiLichtToggleRequest = true;
+				return;
+			}
 
 			if (req != HMI_NONE)	{										// Wenn eine bekannte Eingabekomponente betroffen ist dann...
 				nexGetValue(name);											// ...aktuellen Wert aktiv anfragen
@@ -741,19 +755,19 @@ void nexWertUebernehmen(long value)	{
 				speicherWrite();
 			}
 		} break;
-		case HMI_REQ_DIMM:	{
-			int vorher = dimmlevel;
-			dimmlevel = neuerWert;
-			checkMinMax(dimmlevel, MIN_DIMMLEVEL, MAX_DIMMLEVEL);
-			if (dimmlevel != vorher)	{
-				speicherWrite();
-			}
-		} break;
 		case HMI_REQ_MAXLIGHTTIME:	{
 			int vorher = gwLightTime;
 			gwLightTime = neuerWert;
 			checkMinMax(gwLightTime, MIN_LIGHTTIME, MAX_LIGHTTIME);
 			if (gwLightTime != vorher)	{
+				speicherWrite();
+			}
+		} break;
+		case HMI_REQ_DIMM:	{
+			int vorher = dimmlevel;
+			dimmlevel = neuerWert;
+			checkMinMax(dimmlevel, MIN_DIMMLEVEL, MAX_DIMMLEVEL);
+			if (dimmlevel != vorher)	{
 				speicherWrite();
 			}
 		} break;
@@ -1038,9 +1052,10 @@ void beleuchtung()	{
 	if (vxState == false)  {                								// Wenn laufender Status FALSE dann...
 		vulTime = millis();             									// ...permanent die Laufzeit merken
 	}
-	if ((inputs.TstLicht == true) && (vxOldState == false))	{				// Wenn Taster mit positiver Flanke gedrueckt dann...
+	if (((inputs.TstLicht == true) && (vxOldState == false)) || (hmiLichtToggleRequest == true))	{				// Wenn Taster mit positiver Flanke gedrueckt ODER HMI-Taster dann...
 		vxState = !vxState;													// ... wenn Zustand vorher AUS war, dann invertieren
 	}
+	hmiLichtToggleRequest = false;										// HMI-Anforderung nach Verarbeitung immer zuruecksetzen		
 	if ((millis() - vulTime > ((unsigned long)gwLightTime*1000uL)))	{		// Wenn maximale Einschaltdauer erreicht dann...	
 		vxState = false;													// ...laufender Status "vxState" auf FALSE
 	}
